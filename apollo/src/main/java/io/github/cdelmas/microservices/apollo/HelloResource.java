d9fcea4e5d6cf9b376628e79555fd8f75a33234d
Delmas Cyril <c.delmas@lectra.com>
2016-06-02 13:45:48 +0200

    add license to file headers

--- apollo/src/main/java/io/github/cdelmas/microservices/apollo/HelloResource.java
+++ apollo/src/main/java/io/github/cdelmas/microservices/apollo/HelloResource.java
@@ -1,3 +1,18 @@
+/*
+   Copyright 2016 Cyril Delmas
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 package io.github.cdelmas.microservices.apollo;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -5,9 +20,6 @@ import java.util.concurrent.atomic.AtomicLong;
 import com.spotify.apollo.RequestContext;
 import io.github.cdelmas.spike.common.Message;
 
-/**
- * Created by c.delmas on 02/06/2016.
- */
 public class HelloResource {
 
     private final AtomicLong counter = new AtomicLong(0);
